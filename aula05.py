bd22dcde38140487b0973e01c959bf3f3d3eee61
Gabriel Duarte <gabrielduartesantos97@gmail.com>
2023-11-14 23:04:27 -0300

    Aula 05 | Conversão de Tipos

---
+++ aula05.py
@@ -0,0 +1,26 @@
+# > Conversão de Tipos
+
+idade = '26'
+numero1 = '10'
+numero2 = '20'
+
+print(numero1 + numero2) # concatenou strings
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+# int()
+# str()
+# float()
+# bool()
+
+altura = input('Informe a sua altura: ')
+
+print(altura, type(altura))
+
+altura2 = float(input('Informe a sua altura: '))
+
+print(altura2, type(altura2))
