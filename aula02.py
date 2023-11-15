ab11e0e049a2549322ba7663c803d012c9831bbc
Gabriel Duarte <gabrielduartesantos97@gmail.com>
2023-11-14 22:05:29 -0300

    Aula 02 | Entrada e saída de dados

---
+++ aula02.py
@@ -0,0 +1,6 @@
+#saída de dados
+print('Olá, mundo!')
+print('Seja bem-vindo(a) ao curso de Python do Coding Tank!')
+
+#entrada de dados
+input('Qual é a linguagem de programação que você está estudando? ')
