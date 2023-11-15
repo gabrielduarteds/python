025968594983825e4a6680e42d8395314715af1d
Gabriel Duarte <gabrielduartesantos97@gmail.com>
2023-11-14 22:35:43 -0300

    Aula 03 | Variáveis

---
+++ aula03.py
@@ -0,0 +1,38 @@
+# > VARIÁVEIS
+
+# 1. Variáveis
+
+idade = 26 # Criando uma variável
+
+print(idade)
+
+nome = 'Gabriel Santos'
+
+print(nome)
+
+"""
+Tipos de variáveis
+
+    1. int: números inteiros, ou seja, números sem parte decimal: 0, 5, -1
+    2. float: números reais, ou seja, números com parte decimal: 0.1, 5.2, -1.5, 1000.3
+    3. str: cadeias de caracteres, ou seja, dados textuais (string)
+    4: bool: valores lógicos (booleanos): True or False
+
+"""
+
+idade = 26
+altura = 1.77
+nome = 'Gabriel Santos'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+
+# Obtendo dados do usuário e salvando em variáveis
+
+linguagem = input('Qual é a linguagem de programação que você está estudando? ')
+
+print('A linguagem que você está estudando é ', linguagem)
