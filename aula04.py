aad96cd2a0cd003611d130f0e0e98b422d969db1
Gabriel Duarte <gabrielduartesantos97@gmail.com>
2023-11-14 22:54:28 -0300

    Aula 04 | Operadores Aritméticos e Booleanos

---
+++ aula04.py
@@ -0,0 +1,38 @@
+# > Operações Matemáticas (Aritméticas)
+
+"""
+    - Soma: +
+    - Subtração: -
+    - Multiplicação: *
+    - Divisão: /
+    - Divisão inteira: //
+    - Resto da divisão: %
+    - Potência: **
+
+"""
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2) # ignora a parte decimal da divisão
+print(20 % 3) # resto da divisão de 20 por 3
+print(2 ** 3) # 2 elevado ao cubo
+
+# > Operadores Booleanos 
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)          #False
+print(idade1 <= idade2)         #True
+print('Python' == 'python')     #False
+print('banana' != 'abacaxi')    #True
+print(altura1 >= altura2)       #True
+print(altura2 > altura3)        #False
